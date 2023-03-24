aca17fcc5677ab99321ed2fcbbf00f02cb80ad8e
yasirACTUALY <117306950+yasirACTUALY@users.noreply.github.com>
2023-03-24 19:04:28 -0400

    made a ui and ran commands on the client based on the ui input
    Signed-off-by: yasirACTUALY <117306950+yasirACTUALY@users.noreply.github.com>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Stack, TextField, Typography } from '@mui/material';
@@ -12,11 +12,36 @@ function useDockerDesktopClient() {
 } 
 
 export function App() {
+  const [inputText, setInputText] = useState("");
+  const [inputText2, setInputText2] = useState("");
   const [response, setResponse] = React.useState<string>();
   const ddClient = useDockerDesktopClient();
 
-  const fetchAndDisplayResponse = async () => {
-    const result2 = await ddClient.extension.vm?.cli?.exec("mkdir", [" /mnt/c/Users/yasir/Desktop/done"], {
+  const buttonClicked = async () => {
+    await ddClient.extension.host.cli.exec("kubectl", ["-h"], {
+      stream: {
+        onOutput(data): void {
+            // As we can receive both `stdout` and `stderr`, we wrap them in a JSON object
+            JSON.stringify(
+              {
+                stdout: data.stdout,
+                stderr: data.stderr,
+              },
+              null,
+              "  "
+            );
+        },
+        onError(error: any): void {
+          console.error(error);
+        },
+        onClose(exitCode: number): void {
+          console.log("onClose with exit code " + exitCode);
+        },
+      },
+    });
+
+    
+    const result2 = await ddClient.extension.vm?.cli?.exec(inputText, [inputText2], {
       stream: {
         onOutput(data): void {
           console.log("start 1" + JSON.stringify(data));
@@ -47,12 +72,24 @@ export function App() {
       <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
         The basic page for a terminal in which you can write down a command
       </Typography>
+      <input
+        type="text"
+        id="command type"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <input
+        type="text"
+        id="commands flags"
+        value={inputText2}
+        onChange={(e) => setInputText2(e.target.value)}
+      />
       <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
         Pressing the below button will trigger a request to the backend. Its
         response will appear in the textarea.
       </Typography>
       <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={fetchAndDisplayResponse}>
+        <Button variant="contained" onClick={buttonClicked}>
           Fetch response
         </Button>
 
