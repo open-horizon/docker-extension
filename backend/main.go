04ed66d9ff41c6248b8725bdbddf2b37dd5c5fd2
Demopans <gao.jeff3@gmail.com>
2023-03-24 22:58:59 -0400

    clean up, and try to get this working from prev known working thing
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	_ "fmt"
 	"github.com/labstack/echo/middleware"
 	"log"
 	"net"
@@ -58,53 +57,8 @@ func main() {
 	// hzn(command) // ping oneself
 }
 
-func connect() {
-	var socketPath string
-	flag.StringVar(&socketPath, "socket", "/run/guest-services/backend.sock", "Unix domain socket to listen on")
-	flag.Parse()
-
-	_ = os.RemoveAll(socketPath)
-
-	logger.SetOutput(os.Stdout)
-
-	logMiddleware := middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Skipper: middleware.DefaultSkipper,
-		Format: `{"time":"${time_rfc3339_nano}","id":"${id}",` +
-			`"method":"${method}","uri":"${uri}",` +
-			`"status":${status},"error":"${error}"` +
-			`}` + "\n",
-		CustomTimeFormat: "2006-01-02 15:04:05.00000",
-		Output:           logger.Writer(),
-	})
-
-	logger.Infof("Starting listening on %s\n", socketPath)
-	router := echo.New()
-	router.HideBanner = true
-	router.Use(logMiddleware)
-	startURL := ""
-
-	ln, err := listen(socketPath)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	router.Listener = ln
-
-	router.GET("/hello", hello)
-
-	logger.Fatal(router.Start(startURL))
-}
-
 func listen(path string) (net.Listener, error) {
 	return net.Listen("unix", path)
-}
-
-func hello(ctx echo.Context) error {
-	out, err := exec.Command("ls").Output()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	return ctx.JSON(http.StatusOK, HTTPMessageBody{Message: string(out)})
 }
 
 type HTTPMessageBody struct{ Message string }
